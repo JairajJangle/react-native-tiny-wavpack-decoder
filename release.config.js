b7cd69b764bbb20db55091d6b0b0e9625dd63901
Jairaj Jangle <25704330+JairajJangle@users.noreply.github.com>
2025-04-28 08:06:46 +0530

    chore: release prep

---
+++ release.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  branches: [
+    'main',
+    {
+      name: 'beta',
+      prerelease: true, // Marks this as a prerelease channel
+    },
+  ],
+  plugins: [
+    '@semantic-release/commit-analyzer', // Analyzes commits for version bumping
+    '@semantic-release/release-notes-generator', // Generates release notes
+    '@semantic-release/changelog', // Generates the changelog
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: true,
+        tag: 'beta', // Publishes with a 'beta' tag to npm
+      },
+    ],
+    '@semantic-release/github', // Handles GitHub releases
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'yarn.lock', 'CHANGELOG.md'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
